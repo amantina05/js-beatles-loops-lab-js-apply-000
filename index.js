658b6f5b7fc070d056352a24ffef58ada6473fdd
Amantina Sosa <amantinasosa@gmail.com>
2019-09-20 16:00:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ function iLoveTheBeatles(number) {
   
   let newArray = []; 
   do {
-    newArray.push(`I love the Beatles!`); //this will happen one time, whether or not number meets condition below
-    number++; //increment number of times string above is pushed into newArray
-  } while (number < 15); //(that number subracted from 15 will be the output number of times the string is logged); 
+    newArray.push(`I love the Beatles!`);
+    number++; 
+  } while (number < 15); 
 
   return newArray; // this needs to be outside the loop, otherwise only first item will be returned
 } //end of function
